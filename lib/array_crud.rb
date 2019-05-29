6e6ee1824e45831c9b7e3e2c1406e1267b87b4d7
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-29 20:55:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
-  puts array.push("#{element}")
+  puts array.push("element")
   puts array.inspect
 end
 
