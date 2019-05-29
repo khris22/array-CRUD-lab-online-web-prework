d46670c6cc1f9577bbaa71c6bfccaeda82bace11
Khristian Punzalan <khrispunzalan@gmail.com>
2019-05-29 21:01:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,34 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  colors = ["red", "blue", "black", "yellow"]
-  puts colors.unshift("white")
-  puts colors.inspect
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  colors = ["red", "blue", "black", "yellow"]
-  yellow_color = colors.pop
-  puts colors.inspect
+ array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  colors = ["red", "blue", "black", "yellow"] 
-  red = colors.shift 
-  puts colors.inspect
+ array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  colors = ["red", "blue", "black", "yellow"]
-  colors[2]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  colors = ["red", "blue", "black", "yellow"]
-  colors[0]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  colors = ["red", "blue", "black", "yellow"]
-  colors[-1]
+  array[-1]
 end
